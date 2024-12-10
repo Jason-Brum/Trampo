c9c17a70a6d4e2c3fcf9aaa40c4e7a22e5d79795
Jason-Brum <contato@jasonb.com.br>
2024-12-10 06:56:07 -0300

    ok

---
+++ tailwind/dev_house/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamiliy: {
+        "sans": ["Poppins"]
+      }
+    },
+  },  
+  plugins: [],
+}
+
